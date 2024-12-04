debbd85d84251b93e039fe4c0966efac5fdaff56
hanna.haataja <hanna.haataja@solita.fi>
2024-12-04 09:44:17 +0200

    Solve day 4 add day 6

--- y2024/d04/solution.py
+++ y2024/d04/solution.py
@@ -2,20 +2,121 @@ from aocd.models import Puzzle
 
 
 def parse(data):
-    return data.split()
+    rows = data.split("\n")
+    return rows
+
+
+def check_for_xmas(row_index, col_index, data):
+    # check to right
+    s = 0
+    if col_index + 1 < len(data[row_index]):
+        if data[row_index][col_index + 1] == 'M':
+            if col_index + 2 < len(data[row_index]):
+                if data[row_index][col_index + 2] == 'A':
+                    if col_index + 3 < len(data[row_index]):
+                        if data[row_index][col_index + 3] == 'S':
+                            s += 1
+    # check to left
+    if col_index - 1 >= 0:
+        if data[row_index][col_index - 1] == 'M':
+            if col_index - 2 >= 0:
+                if data[row_index][col_index - 2] == 'A':
+                    if col_index - 3 >= 0:
+                        if data[row_index][col_index - 3] == 'S':
+                            s += 1
+    # check up
+    if row_index - 1 >= 0:
+        if data[row_index - 1][col_index] == 'M':
+            if row_index - 2 >= 0:
+                if data[row_index - 2][col_index] == 'A':
+                    if row_index - 3 >= 0:
+                        if data[row_index - 3][col_index] == 'S':
+                            s += 1
+    # check down
+    if row_index + 1 < len(data):
+        if data[row_index + 1][col_index] == 'M':
+            if row_index + 2 < len(data):
+                if data[row_index + 2][col_index] == 'A':
+                    if row_index + 3 < len(data):
+                        if data[row_index + 3][col_index] == 'S':
+                            s += 1
+    # check diagonal right up
+    if col_index + 1 < len(data[row_index]) and row_index - 1 >= 0:
+        if data[row_index - 1][col_index + 1] == 'M':
+            if col_index + 2 < len(data[row_index]) and row_index - 2 >= 0:
+                if data[row_index - 2][col_index + 2] == 'A':
+                    if col_index + 3 < len(data[row_index]) and row_index - 3 >= 0:
+                        if data[row_index - 3][col_index + 3] == 'S':
+                            s += 1
+    # check diagonal right down
+    if col_index + 1 < len(data[row_index]) and row_index + 1 < len(data):
+        if data[row_index + 1][col_index + 1] == 'M':
+            if col_index + 2 < len(data[row_index]) and row_index + 2 < len(data):
+                if data[row_index + 2][col_index + 2] == 'A':
+                    if col_index + 3 < len(data[row_index]) and row_index + 3 < len(data):
+                        if data[row_index + 3][col_index + 3] == 'S':
+                            s += 1
+    # check diagonal left up
+    if col_index - 1 >= 0 and row_index - 1 >= 0:
+        if data[row_index - 1][col_index - 1] == 'M':
+            if col_index - 2 >= 0 and row_index - 2 >= 0:
+                if data[row_index - 2][col_index - 2] == 'A':
+                    if col_index - 3 >= 0 and row_index - 3 >= 0:
+                        if data[row_index - 3][col_index - 3] == 'S':
+                            s += 1
+    # check diagonal left down
+    if col_index - 1 >= 0 and row_index + 1 < len(data):
+        if data[row_index + 1][col_index - 1] == 'M':
+            if col_index - 2 >= 0 and row_index + 2 < len(data):
+                if data[row_index + 2][col_index - 2] == 'A':
+                    if col_index - 3 >= 0 and row_index + 3 < len(data):
+                        if data[row_index + 3][col_index - 3] == 'S':
+                            s += 1
+    # print("{} {} {} ".format(row_index, col_index, s))
+    return s
 
 
 def part1(data):
     """Solve part 1."""
     number_sum = 0
+    for row_index in range(len(data)):
+        # print(data[row_index])
+        for col_index in range(len(data[row_index])):
+            if data[row_index][col_index] == 'X':
+                number_sum += check_for_xmas(row_index, col_index, data)
 
     return number_sum
+
+
+def check_for_x(row_index, col_index, data):
+    s = 0
+    if row_index - 1 >= 0 and col_index - 1 >= 0:
+        left_up_corner = data[row_index - 1][col_index - 1]
+        if left_up_corner == 'M' or left_up_corner == 'S':
+            right_down_corner = 'S'
+            if left_up_corner == 'S':
+                right_down_corner = 'M'
+            if row_index + 1 < len(data) and col_index + 1 < len(data[row_index]):
+                if data[row_index + 1][col_index + 1] == right_down_corner:
+                    if col_index + 1 < len(data[row_index]):
+                        right_up_corner = data[row_index - 1][col_index + 1]
+                        if right_up_corner == 'M' or right_up_corner == 'S':
+                            left_down_corner = 'S'
+                            if right_up_corner == 'S':
+                                left_down_corner = 'M'
+                            if data[row_index + 1][col_index - 1] == left_down_corner:
+                                s += 1
+
+    return s
 
 
 def part2(data):
     """Solve part 2."""
     number_sum = 0
-
+    for row_index in range(len(data)):
+        for col_index in range(len(data[row_index])):
+            if data[row_index][col_index] == 'A':
+                number_sum += check_for_x(row_index, col_index, data)
     return number_sum
 
 
@@ -29,16 +130,24 @@ def solve(puzzle_data):
 
 
 def test():
-    test1_data = parse(puzzle.examples[0].input_data)
+    test1_data = parse("""MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX""")
     solution1 = part1(test1_data)
-    if solution1 == int(puzzle.examples[0].answer_a):
+    if solution1 == int(18):
         print("TEST 1: YES!")
     else:
         print("was {} should have been {}".format(solution1, puzzle.examples[0].answer_a))
 
-    test2_data = parse(puzzle.examples[0].input_data)
-    solution2 = part2(test2_data)
-    if solution2 == int(31):
+    solution2 = part2(test1_data)
+    if solution2 == int(9):
         print("TEST 2: YES!")
     else:
         print("was {} should have been {}".format(solution2, 31))
