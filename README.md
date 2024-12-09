1e86226e5ad96ffa0e91b74242aa5399fefd05e9
hanna.haataja <hanna.haataja@solita.fi>
2024-12-09 11:05:01 +0200

    Solve day 9 add day 11

--- README.md
+++ README.md
@@ -14,3 +14,4 @@ I am total rookie with this language so challenging my self and learning new is 
 - Guard Gallivant (1/2)
 - Bridge Repair
 - Resonant Collinearity
+- Disk Fragmenter
--- y2024/d09/solution.py
+++ y2024/d09/solution.py
@@ -2,19 +2,89 @@ from aocd.models import Puzzle
 
 
 def parse(data):
-    return data.split()
+    id_number = 0
+    is_space = False
+    checksum_list = []
+    for d in data:
+        d_number = int(d)
+        for number in range(d_number):
+            if is_space:
+                checksum_list.append(".")
+            else:
+                checksum_list.append(id_number)
+        if not is_space:
+            id_number += 1
+        is_space = not is_space
+    return checksum_list
 
 
 def part1(data):
+    checksum_list = data.copy()
     """Solve part 1."""
     number_sum = 0
-
+    print("start part 1")
+    while checksum_list.count(".") > 0:
+        last = checksum_list.pop()
+        if last != ".":
+            index = checksum_list.index(".")
+            del checksum_list[index]
+            checksum_list.insert(index, last)
+    print("list ordered, calculating checksum")
+    for cs_index in range(len(checksum_list)):
+        number_sum += cs_index * checksum_list[cs_index]
+    print("Check sum calculated")
     return number_sum
 
 
+def remove_old_instances(number, checksum_list):
+    for index in range(len(checksum_list)):
+        if checksum_list[index] == number:
+            checksum_list[index] = "."
+
+
+def search_and_replace(number, count, index, checksum_list):
+    search = checksum_list[index:]
+    search.reverse()
+    search_count = 0
+    for search_index in range(len(search)):
+        if search[search_index] == ".":
+            search_count += 1
+            # print("{} searching {}".format(search_index, search_count))
+            if search_count == count:
+                remove_old_instances(number, checksum_list)
+                for insert_number in range(count):
+                    replace_index = - search_index - 1 + insert_number
+                    # print("index: {}, replace index {},".format(search_index, replace_index))
+                    del checksum_list[replace_index]
+                    checksum_list.insert(replace_index + 1, number)
+                break
+
+        else:
+            search_count = 0
+
+
 def part2(data):
+    checksum_list = data.copy()
     """Solve part 2."""
     number_sum = 0
+    print("start part 2".format(data))
+
+    checksum_list.reverse()
+    current = 0
+    current_count = 1
+    for index in range(len(checksum_list) - 1):
+        current = checksum_list[index]
+        if checksum_list[index + 1] != current:
+            search_and_replace(current, current_count, index, checksum_list)
+            # print("replaced {} {} : {}".format(current, current_count, checksum_list))
+            current_count = 1
+        else:
+            current_count += 1
+    checksum_list.reverse()
+    print("Got in order")
+    for index in range(len(checksum_list)):
+        if checksum_list[index] != ".":
+            number_sum += index * checksum_list[index]
 
     return number_sum
 
@@ -38,14 +108,14 @@ def test():
 
     test2_data = parse(puzzle.examples[0].input_data)
     solution2 = part2(test2_data)
-    if solution2 == int(31):
+    if solution2 == int(2858):
         print("TEST 2: YES!")
     else:
-        print("was {} should have been {}".format(solution2, 31))
+        print("was {} should have been {}".format(solution2, 2858))
 
 
 if __name__ == "__main__":
-    puzzle = Puzzle(year=2024, day=8)
+    puzzle = Puzzle(year=2024, day=9)
     print(puzzle.examples)
     test()
 
