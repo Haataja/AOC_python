7f9ae46d01da3af0f561109b74bc13f26c4c872d
hanna.haataja <hanna.haataja@solita.fi>
2024-12-11 20:21:36 +0200

    Solve day 11 part 1

--- y2024/d11/solution.py
+++ y2024/d11/solution.py
@@ -2,14 +2,42 @@ from aocd.models import Puzzle
 
 
 def parse(data):
-    return data.split()
+    return list(map(lambda x: int(x), data.split()))
 
 
-def part1(data):
+def part1(data, blinks):
     """Solve part 1."""
     number_sum = 0
-
-    return number_sum
+    # print(data)
+    for index in range(blinks):
+        print(index)
+        stone_index = 0
+        while stone_index < len(data):
+            if data[stone_index] == 0:
+                data[stone_index] = 1
+                # print("ADDED 1 to zero: {}".format(data))
+            elif len(str(data[stone_index])) % 2 == 0:
+                str_stone = str(data[stone_index])
+                half = int(len(str_stone) / 2)
+                first_stone = int(str_stone[:half])
+                second_stone = int(str_stone[half:])
+                # print(first_stone, second_stone)
+                del data[stone_index]
+                if stone_index == 0:
+                    data.insert(stone_index, first_stone)
+                    data.insert(1, second_stone)
+                else:
+                    data.insert(stone_index, second_stone)
+                    data.insert(stone_index, first_stone)
+                if stone_index + 1 < len(data):
+                    stone_index = stone_index + 1
+                # print("ADDED stone: {}".format(data))
+            else:
+                data[stone_index] = data[stone_index] * 2024
+                # print("Multiplied by 2024: {}".format(data))
+            stone_index += 1
+    # print(data)
+    return len(data)
 
 
 def part2(data):
@@ -22,21 +50,24 @@ def part2(data):
 def solve(puzzle_data):
     """Solve the puzzle for the given input."""
     data = parse(puzzle_data)
-    solution1 = part1(data)
-    solution2 = part2(data)
+    data1 = data.copy()
+    solution1 = part1(data1, 25)
+    data2 = data.copy()
+    solution2 = part2(data2)
 
     return solution1, solution2
 
 
 def test():
-    test1_data = parse(puzzle.examples[0].input_data)
-    solution1 = part1(test1_data)
+    orig_data = parse("125 17")
+    test1_data = orig_data.copy()
+    solution1 = part1(test1_data, 25)
     if solution1 == int(puzzle.examples[0].answer_a):
         print("TEST 1: YES!")
     else:
         print("was {} should have been {}".format(solution1, puzzle.examples[0].answer_a))
 
-    test2_data = parse(puzzle.examples[0].input_data)
+    test2_data = parse("")
     solution2 = part2(test2_data)
     if solution2 == int(31):
         print("TEST 2: YES!")
